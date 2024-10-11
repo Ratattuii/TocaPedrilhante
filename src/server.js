cfbec4c3434e979d467684836f8c738175e7dd21
ratatui <arthurselegarfritsch>
2024-10-11 09:12:44 -0300

    documentação server.js

--- src/server.js
+++ src/server.js
@@ -1,14 +1,81 @@
 const express = require('express');
 const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
 const app = express();
 const port = 3000;
 const db = require('./dbconfig')
+
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
 // ----------------------------------------------------------------------------
+
+// Configuração do Swagger
+const swaggerOptions = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'TocaPedrilhante API',
+            version: '1.0.0',
+            description: 'API para gerenciar produtos e usuários da loja TocaPedrilhante',
+        },
+        servers: [
+            {
+                url: 'http://localhost:3000',
+            },
+        ],
+    },
+    apis: ['./server.js'],
+};
+
+const swaggerDocs = swaggerJsdoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+// ----------------------------------------------------------------------------
+
+/**
+ * @swagger
+ * /api/produtos:
+ *   get:
+ *     summary: Lista todos os produtos
+ *     responses:
+ *       200:
+ *         description: Retorna uma lista de produtos
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 sucesso:
+ *                   type: boolean
+ *                   example: true
+ *                 message:
+ *                   type: string
+ *                   example: Produtos listados com sucesso
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: integer
+ *                         example: 1
+ *                       nome:
+ *                         type: string
+ *                         example: "Produto A"
+ *                       preco:
+ *                         type: number
+ *                         example: 10.99
+ *                       descricao:
+ *                         type: string
+ *                         example: "Descrição do produto A"
+ *                       imagem:
+ *                         type: string
+ *                         example: "/path/to/image.jpg"
+ */
 
 // Rota para buscar todos os produtos
 app.get('/api/produtos', (req, res) => {
@@ -25,6 +92,47 @@ app.get('/api/produtos', (req, res) => {
 // ----------------------------------------------------------------------------
 
 // Rota para buscar um produto específico pelo id
+
+/**
+ * @swagger
+ * /api/produto/{id}:
+ *   get:
+ *     summary: Busca um produto pelo ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID do produto
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Retorna os dados do produto
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 sucesso:
+ *                   type: boolean
+ *                   example: true
+ *                 message:
+ *                   type: string
+ *                   example: Produto listado com sucesso
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     nome:
+ *                       type: string
+ *                       example: "Produto A"
+ *                     preco:
+ *                       type: number
+ *                       example: 10.99
+ *                     descricao:
+ *                       type: string
+ *                       example: "Descrição do produto A"
+ */
+
 app.get('/api/produto/:id', (req, res) => {
     const { id } = req.params;
 
@@ -42,6 +150,33 @@ app.get('/api/produto/:id', (req, res) => {
 
 // ----------------------------------------------------------------------------
 
+/**
+ * @swagger
+ * /api/produto:
+ *   post:
+ *     summary: Adiciona um novo produto
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               nome:
+ *                 type: string
+ *               preco:
+ *                 type: number
+ *               descricao:
+ *                 type: string
+ *               imagem:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Produto adicionado com sucesso
+ *       400:
+ *         description: Erro ao adicionar produto
+ */
+
 // Rota para adicionar um novo produto
 app.post('/api/produto', (req, res) => {
     const { nome, preco, descricao, imagem } = req.body;
@@ -57,6 +192,37 @@ app.post('/api/produto', (req, res) => {
 });
 
 // ----------------------------------------------------------------------------
+
+/**
+ * @swagger
+ * /api/produto/{id}:
+ *   put:
+ *     summary: Edita um produto existente
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               nome:
+ *                 type: string
+ *               preco:
+ *                 type: number
+ *               descricao:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Produto editado com sucesso
+ *       400:
+ *         description: Erro ao editar produto
+ */
 
 // Rota para editar um produto existente
 app.put('/api/produto/:id', (req, res) => {
@@ -74,6 +240,24 @@ app.put('/api/produto/:id', (req, res) => {
 
 // ----------------------------------------------------------------------------
 
+/**
+ * @swagger
+ * /api/produto/{id}:
+ *   delete:
+ *     summary: Remove um produto pelo ID
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Produto removido com sucesso
+ *       400:
+ *         description: Erro ao remover produto
+ */
+
 // Rota para remover um produto
 app.delete('/api/produto/:id', (req, res) => {
     const { id } = req.params;
@@ -89,7 +273,36 @@ app.delete('/api/produto/:id', (req, res) => {
 
 // ----------------------------------------------------------------------------
 
-// Endpoints de usuários (Cadastro e Login)
+// Cadastrar usuários
+
+/**
+ * @swagger
+ * /api/usuarios/cadastrar:
+ *   post:
+ *     summary: Cadastra um novo usuário
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               nome:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               senha:
+ *                 type: string
+ *               adm:
+ *                 type: boolean
+ *     responses:
+ *       200:
+ *         description: Usuário cadastrado com sucesso
+ *       400:
+ *         description: Erro ao cadastrar usuário
+ */
+
+
 app.post('/api/usuarios/cadastrar', (req, res) => {
     const { nome, email, senha, adm } = req.body;
     db.query('INSERT INTO Usuarios (nome, email, senha, adm) VALUES (?, ?, ?, ?)', [nome, email, senha, adm], (err, results) => {
@@ -102,6 +315,31 @@ app.post('/api/usuarios/cadastrar', (req, res) => {
 });
 
 // ----------------------------------------------------------------------------
+
+// Login de usuário
+
+/**
+ * @swagger
+ * /api/usuarios/login:
+ *   post:
+ *     summary: Realiza o login do usuário
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               senha:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Login realizado com sucesso
+ *       401:
+ *         description: Credenciais inválidas
+ */
 
 app.post('/api/usuarios/login', (req, res) => {
     const { email, senha } = req.body;
@@ -123,7 +361,33 @@ app.post('/api/usuarios/login', (req, res) => {
 
 // ----------------------------------------------------------------------------
 
-// Endpoints do carrinho
+// Adicionar produto carrinho
+
+/**
+ * @swagger
+ * /api/carrinho:
+ *   post:
+ *     summary: Adiciona produto ao carrinho ou atualiza a quantidade
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               usuario_id:
+ *                 type: integer
+ *               produto_id:
+ *                 type: integer
+ *               quantidade:
+ *                 type: integer
+ *     responses:
+ *       200:
+ *         description: Produto adicionado ou atualizado no carrinho
+ *       400:
+ *         description: Erro ao adicionar ou atualizar carrinho
+ */
+
 app.post('/api/carrinho', (req, res) => {
     const { usuario_id, produto_id, quantidade } = req.body;
 
@@ -154,6 +418,26 @@ app.post('/api/carrinho', (req, res) => {
 
 // ----------------------------------------------------------------------------
 
+// Exibir carrinho do usuário
+
+/**
+ * @swagger
+ * /api/carrinho/{usuario_id}:
+ *   get:
+ *     summary: Exibe o carrinho de um usuário
+ *     parameters:
+ *       - name: usuario_id
+ *         in: path
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Carrinho listado com sucesso
+ *       400:
+ *         description: Erro ao exibir o carrinho
+ */
+
 app.get('/api/carrinho/:usuario_id', (req, res) => {
     const { usuario_id } = req.params;
     db.query('SELECT c.*, p.nome, p.preco, c.quantidade, (p.preco * c.quantidade) AS total FROM Carrinho c JOIN Produtos p ON c.produto_id = p.id WHERE c.usuario_id = ?', [usuario_id], (err, results) => {
@@ -166,6 +450,31 @@ app.get('/api/carrinho/:usuario_id', (req, res) => {
 });
 
 // ----------------------------------------------------------------------------
+
+// Deletar produto do carrinho
+
+/**
+ * @swagger
+ * /api/carrinho/{usuario_id}/{produto_id}:
+ *   delete:
+ *     summary: Remove um produto do carrinho
+ *     parameters:
+ *       - name: usuario_id
+ *         in: path
+ *         required: true
+ *         schema:
+ *           type: integer
+ *       - name: produto_id
+ *         in: path
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Produto removido do carrinho
+ *       400:
+ *         description: Erro ao remover produto do carrinho
+ */
 
 app.delete('/api/carrinho/:usuario_id/:produto_id', (req, res) => {
     const { usuario_id, produto_id } = req.params;
@@ -184,6 +493,25 @@ app.delete('/api/carrinho/:usuario_id/:produto_id', (req, res) => {
 // ----------------------------------------------------------------------------
 
 // Rota para obter os favoritos de um usuário
+
+/**
+ * @swagger
+ * /api/favoritos/{usuario_id}:
+ *   get:
+ *     summary: Obtém os produtos favoritos de um usuário
+ *     parameters:
+ *       - name: usuario_id
+ *         in: path
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Favoritos listados com sucesso
+ *       400:
+ *         description: Erro ao listar favoritos
+ */
+
 app.get('/api/favoritos/:usuario_id', (req, res) => {
     const { usuario_id } = req.params;
 
@@ -201,6 +529,30 @@ app.get('/api/favoritos/:usuario_id', (req, res) => {
 // ----------------------------------------------------------------------------
 
 // Rota para favoritar um produto
+
+/**
+ * @swagger
+ * /api/favoritos:
+ *   post:
+ *     summary: Adiciona um produto aos favoritos
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               usuario_id:
+ *                 type: integer
+ *               produto_id:
+ *                 type: integer
+ *     responses:
+ *       200:
+ *         description: Produto adicionado aos favoritos
+ *       400:
+ *         description: Erro ao adicionar favorito
+ */
+
 app.post('/api/favoritos', (req, res) => {
     const { usuario_id, produto_id } = req.body;
 
@@ -218,6 +570,31 @@ app.post('/api/favoritos', (req, res) => {
 // ----------------------------------------------------------------------------
 
 // Rota para desfavoritar um produto
+
+/**
+ * @swagger
+ * /api/favoritos/{usuario_id}/{produto_id}:
+ *   delete:
+ *     summary: Remove um produto dos favoritos
+ *
+ *  *     parameters:
+ *       - name: usuario_id
+ *         in: path
+ *         required: true
+ *         schema:
+ *           type: integer
+ *       - name: produto_id
+ *         in: path
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Produto removido dos favoritos
+ *       400:
+ *         description: Erro ao remover favorito
+ */
+
 app.delete('/api/favoritos/:usuario_id/:produto_id', (req, res) => {
     const { usuario_id, produto_id } = req.params;
 
@@ -233,6 +610,35 @@ app.delete('/api/favoritos/:usuario_id/:produto_id', (req, res) => {
 });
 
 // ----------------------------------------------------------------------------
+
+/**
+ * @swagger
+ * /api/finalizar-compra:
+ *   post:
+ *     summary: Finaliza a compra de um usuário
+ *     description: Processa os itens do carrinho de um usuário, move os itens para a tabela de compras e limpa o carrinho.
+ *     tags:
+ *       - Carrinho
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - usuario_id
+ *             properties:
+ *               usuario_id:
+ *                 type: int
+ *                 description: ID do usuário que está finalizando a compra.
+ *     responses:
+ *       200:
+ *         description: Compra finalizada com sucesso, carrinho foi limpo.
+ *       400:
+ *         description: O carrinho está vazio ou houve um erro na compra.
+ *       500:
+ *         description: Erro no processamento da compra ou na limpeza do carrinho.
+ */
 
 app.post('/api/finalizar-compra', (req, res) => {
     const { usuario_id } = req.body;
