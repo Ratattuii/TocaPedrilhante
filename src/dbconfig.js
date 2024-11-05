83e18f962b61420b6bef7bb18fb254a96eac59ae
Ratatui <arthurselegarfritsch@gmail.com>
2024-11-05 16:08:42 -0300

    render.yaml

--- src/dbconfig.js
+++ src/dbconfig.js
@@ -1,14 +1,14 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'tocapedrilhante',
-    password: 'tocapedrilhante',
-    database: 'TocaPedrilhante'
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
 });
 
 db.connect((err) => {
-    if(err) {
+    if (err) {
         throw err;
     } else {
         console.log('MySQL conectado');
