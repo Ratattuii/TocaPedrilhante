041d68154f1f7a2e12cb9d9d90cf92a281568b81
Ratatui <arthurselegarfritsch@gmail.com>
2024-11-06 18:46:00 -0300

    commit

--- src/dbconfig.js
+++ src/dbconfig.js
@@ -1,12 +1,20 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-    host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT || 3306,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
+    host: 'bxqqfmfsq9wwiddwznzj-mysql.services.clever-cloud.com',
+    port: 3306,
+    user: 'uwfgvvtrntb5tysj',
+    password: 'DS9Bw9Hf6xrC6pB0eZKl',
+    database: 'bxqqfmfsq9wwiddwznzj'
 });
+
+// const db = mysql.createConnection({
+//     host: process.env.DATABASE_HOST,
+//     port: process.env.DATABASE_PORT || 3306,
+//     user: process.env.DATABASE_USER,
+//     password: process.env.DATABASE_PASSWORD,
+//     database: process.env.DATABASE_NAME,
+// });
 
 db.connect((err) => {
     if (err) {
